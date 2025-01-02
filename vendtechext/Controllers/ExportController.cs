b98e0603ce9979a729c33080b4f16233c58a7fe3
flavetechs <104333668+flavetechs@users.noreply.github.com>
2025-01-02 04:27:47 +0100

    fix export

--- vendtechext/Controllers/ExportController.cs
+++ vendtechext/Controllers/ExportController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
 using vendtechext.BLL.Interfaces;
@@ -45,29 +46,29 @@ namespace vendtechext.Controllers
 
                     // Add headers
                     worksheet.Cells[1, 1].Value = "DATE";
-                    worksheet.Cells[1, 2].Value = "INTEGRATOR";
-                    worksheet.Cells[1, 3].Value = "WALLETE ID";
-                    worksheet.Cells[1, 4].Value = "TRANX ID";
-                    worksheet.Cells[1, 5].Value = "VTECH ID";
-                    worksheet.Cells[1, 6].Value = "STATUS";
-                    worksheet.Cells[1, 7].Value = "METER";
-                    worksheet.Cells[1, 8].Value = "BALANCE BEFORE";
-                    worksheet.Cells[1, 9].Value = "AMOUNT";
-                    worksheet.Cells[1, 10].Value = "BAL AFTER";
+                    //worksheet.Cells[1, 2].Value = "INTEGRATOR";
+                    //worksheet.Cells[1, 3].Value = "WALLETE ID";
+                    worksheet.Cells[1, 2].Value = "TRANX ID";
+                    worksheet.Cells[1, 3].Value = "VTECH ID";
+                    worksheet.Cells[1, 4].Value = "STATUS";
+                    worksheet.Cells[1, 5].Value = "METER";
+                    worksheet.Cells[1, 6].Value = "BALANCE BEFORE";
+                    worksheet.Cells[1, 7].Value = "AMOUNT";
+                    worksheet.Cells[1, 8].Value = "BAL AFTER";
 
                     // Add data
                     for (int i = 0; i < transactions.Count; i++)
                     {
                         worksheet.Cells[i + 2, 1].Value = transactions[i].Date;
-                        worksheet.Cells[i + 2, 2].Value = transactions[i].Integrator;
-                        worksheet.Cells[i + 2, 3].Value = transactions[i].WalletId;
-                        worksheet.Cells[i + 2, 4].Value = transactions[i].TransactionId;
-                        worksheet.Cells[i + 2, 5].Value = transactions[i].VendtechTransactionID;
-                        worksheet.Cells[i + 2, 6].Value = transactions[i].Status;
-                        worksheet.Cells[i + 2, 7].Value = transactions[i].MeterNumber;
-                        worksheet.Cells[i + 2, 8].Value = transactions[i].BalanceBefore;
-                        worksheet.Cells[i + 2, 9].Value = transactions[i].Amount;
-                        worksheet.Cells[i + 2, 10].Value = transactions[i].BalanceAfter;
+                        //worksheet.Cells[i + 2, 2].Value = transactions[i].Integrator;
+                        //worksheet.Cells[i + 2, 3].Value = transactions[i].WalletId;
+                        worksheet.Cells[i + 2, 2].Value = transactions[i].TransactionId;
+                        worksheet.Cells[i + 2, 3].Value = transactions[i].VendtechTransactionID;
+                        worksheet.Cells[i + 2, 4].Value = transactions[i].Status;
+                        worksheet.Cells[i + 2, 5].Value = transactions[i].MeterNumber;
+                        worksheet.Cells[i + 2, 6].Value = transactions[i].BalanceBefore;
+                        worksheet.Cells[i + 2, 7].Value = transactions[i].Amount;
+                        worksheet.Cells[i + 2, 8].Value = transactions[i].BalanceAfter;
                     }
 
                     var stream = new MemoryStream();
@@ -80,16 +81,17 @@ namespace vendtechext.Controllers
             else if (format == "pdf")
             {
                 // Generate PDF
-                var stream = new MemoryStream();  // Do not use "using" here
+                var stream = new MemoryStream();
                 var document = new iTextSharp.text.Document();
                 var writer = iTextSharp.text.pdf.PdfWriter.GetInstance(document, stream);
                 document.Open();
 
                 // Add headers
                 var table = new iTextSharp.text.pdf.PdfPTable(12);
+
                 table.AddCell("DATE");
-                table.AddCell("INTEGRATOR");
-                table.AddCell("WALLETE ID");
+                //table.AddCell("INTEGRATOR");
+                //table.AddCell("WALLETE ID");
                 table.AddCell("TRANX ID");
                 table.AddCell("VTECH ID");
                 table.AddCell("STATUS");
@@ -102,8 +104,8 @@ namespace vendtechext.Controllers
                 foreach (var transaction in transactions)
                 {
                     table.AddCell(transaction.Date);
-                    table.AddCell(transaction.Integrator);
-                    table.AddCell(transaction.WalletId);
+                    //table.AddCell(transaction.Integrator);
+                    //table.AddCell(transaction.WalletId);
                     table.AddCell(transaction.TransactionId);
                     table.AddCell(transaction.VendtechTransactionID);
                     table.AddCell(transaction.Status);
