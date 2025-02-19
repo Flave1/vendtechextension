34620d606ba60ddd78e0c0d6d427e6720708e7e6
flavetechs <104333668+flavetechs@users.noreply.github.com>
2025-02-19 17:30:54 +0100

    fix conflict

--- vendtechext.BLL/Repository/TransactionRepository.cs
+++ vendtechext.BLL/Repository/TransactionRepository.cs
@@ -150,22 +150,6 @@ namespace vendtechext.BLL.Repository
             await _context.SaveChangesAsync();
         }
 
-        public async Task UpdateSaleSuccessTransactionLogSANDBOX(Transaction trans)
-        {
-            new TransactionsBuilder(trans)
-                .WithSellerReturnedBalance(trans.SellerReturnedBalance)
-                .WithVendStatusDescription(trans.VendStatusDescription ?? "")
-                .WithSellerTransactionId(trans.SellerTransactionID ?? "")
-                .WithTransactionStatus(TransactionStatus.Success)
-                .WithReceivedFrom(trans.ReceivedFrom)
-                .WithResponse(trans.Response)
-                .WithRequest(trans.Request)
-                .WithFinalized(true)
-                .Build();
-
-            await _context.SaveChangesAsync();
-        }
-
         public async Task UpdateSaleTransactionLogOnStatusQuery(ExecutionResult executionResult, Transaction trans, TransactionStatus status)
         {
             new TransactionsBuilder(trans)
